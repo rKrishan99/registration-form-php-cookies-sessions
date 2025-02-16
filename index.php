5b313727ef89a81a03f8e421b763eee42ac8c1d3
RajithaKWG <rkrishan894@gmail.com>
2025-02-16 17:05:06 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include 'topmenu.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+   <h1 class="container text-center">Registration and Login in PHP and MySQL</h1>
+
+</body>
+
+</html>
