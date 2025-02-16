ef1183a625d4ca5bff054fa5d13d48de8a012431
RajithaKWG <rkrishan894@gmail.com>
2025-02-16 17:35:59 +0530

    created header, register form and login form

---
+++ registor.php
@@ -0,0 +1,54 @@
+<?php
+include 'topmenu.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="d-flex justify-content-center" style="margin-top: 5rem;">
+        <form style="width: 30rem; display: flex; flex-direction:column; gap: 1rem;">
+            <h1 class="h3 mb-3 fw-normal">Please Register</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Name</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="form-check text-start my-3">
+                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    show password
+                </label>
+            </div>
+            <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
+            <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2025</p>
+        </form>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
